587c8ef77ba68bc18ffb665b762815d62e3c7f92
fumi uk <dcfumiya.238@gmail.com>
2025-04-29 12:57:13 +0900

    Add Projects page and enhance layout with color mode support
    - Implemented Projects page with project listing and loading/error states.
    - Updated layout to include a color mode selector and improved header structure.
    - Added color mode module to project dependencies and configured Tailwind CSS for dark mode.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="container mx-auto max-w-2xl">
     <header class="flex justify-between items-center mt-5">
-      <div>
-        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200"
-          >Orakko</NuxtLink
-        >
+      <div class="flex items-center space-x-12">
+        <div>
+          <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200"
+            >Orakko</NuxtLink
+          >
+        </div>
+        <Menu />
       </div>
-      <Menu />
+
+      <ClientOnly>
+        <ColorModeSelector />
+      </ClientOnly>
     </header>
-    <main class="p-2">
+    <main class="p-2 mt-10">
       <slot />
     </main>
   </div>
@@ -33,5 +39,9 @@ useHead({
 <style>
 body {
   font-family: "Roboto", sans-serif;
+}
+
+body {
+  @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300;
 }
 </style>
