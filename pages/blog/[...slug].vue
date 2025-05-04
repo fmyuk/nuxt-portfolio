cce26852a8776f856339ce47c50a70873e188975
fumi uk <dcfumiya.238@gmail.com>
2025-05-04 11:39:20 +0900

    feat: improve transition effects, enhance blog and project list styling, and add dark mode support

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -29,6 +29,7 @@
 const activeId = ref(null);
 
 onMounted(() => {
+  let elements = [];
   const callback = (entries) => {
     for (const entry of entries) {
       if (entry.isIntersecting) {
@@ -42,12 +43,13 @@ onMounted(() => {
     root: null,
     threshold: 0.5,
   });
-  const elements = document.querySelectorAll("h2, h3");
+  setTimeout(() => {
+    elements = document.querySelectorAll("h2, h3");
 
-  for (const element of elements) {
-    observer.observe(element);
-  }
-
+    for (const element of elements) {
+      observer.observe(element);
+    }
+  }, 150);
   onBeforeUnmount(() => {
     for (const element of elements) {
       observer.unobserve(element);
